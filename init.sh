c7dfc1ae0f5e44695d37d2a315eb55201d3b3a8a
zhliu <zhliu0106@gmail.com>
2021-11-20 16:37:45 +0800

    update random init port

--- init.sh
+++ init.sh
@@ -4,12 +4,24 @@ bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/
 # 更新config文件
 wget https://raw.githubusercontent.com/zhliu0106/v2ray-config/main/config.json  -O -> /usr/local/etc/v2ray/config.json
 
-# 随机初始化123与456端口的shadowsocks密码
+# 随机初始化端口与shadowsocks密码
+function rand(){
+  min=$1
+  max=$(($2-$min+1))
+  num=$(($RANDOM+1000000000))
+  echo $(($num%$max+$min))
+}
+
 pw_123=$(openssl rand -hex 4)
 pw_456=$(openssl rand -hex 4)
+port_123=$(rand 1001 2000)
+port_456=$(rand 2001 3000)
 
 sed -i "s/pw_123/$pw_123/g" /usr/local/etc/v2ray/config.json
 sed -i "s/pw_456/$pw_456/g" /usr/locab/etc/v2ray/config.json
+sed -i "s/port_123/$port_123/g" /usr/local/etc/v2ray/config.json
+sed -i "s/port_456/$port_456/g" /usr/local/etc/v2ray/config.json
+
 
 # 测试config
 /usr/local/bin/v2ray -test -config /usr/local/etc/v2ray/config.json
@@ -22,5 +34,5 @@ service v2ray restart
 
 # 输出端口密码
 echo -e "protocol:shadowsocks\nmethod:aes-256-gcm\n"
-echo -e "port:123\tpassword:$pw_123\n"
-echo -e "port:456\tpassword:$pw_456\n"
+echo -e "port:$port_123\tpassword:$pw_123\n"
+echo -e "port:$port_456\tpassword:$pw_456\n"
